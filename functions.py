f01b50442feb9c01a93b49332bb6d45aa74434ed
EV4 <49408883+EV4g@users.noreply.github.com>
2026-02-26 17:55:10 +0100

    'new' gaussian fit functions

--- functions.py
+++ functions.py
@@ -5,9 +5,7 @@ from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 import numpy as np
-from reproject import reproject_interp
-import matplotlib.pyplot as plt
-import glob
+from scipy.optimize import curve_fit
 
 warnings.filterwarnings("ignore", category=FITSFixedWarning)
 
@@ -63,3 +61,58 @@ def generate_new_wcs(center, nx, ny, pixscale):
     wcs_out.wcs.crpix = [(nx + 1) / 2.0, (ny + 1) / 2.0]
     wcs_out.wcs.cdelt = [-pixscale.to_value(u.deg), pixscale.to_value(u.deg)]
     return wcs_out
+
+"""A 2D gaussian function, used for fitting point sources"""
+def twoD_Gaussian(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):    
+    x, y = xy
+    
+    dx, dy = x - xo, y - yo
+    sint_sq, cost_sq = np.sin(theta)**2, np.cos(theta)**2
+    two_sig_x_sq, two_sig_y_sq = 2*sigma_x**2, 2*sigma_y**2
+    
+    a = cost_sq / two_sig_x_sq + sint_sq / two_sig_y_sq
+    b = -np.sin(2*theta) / (2 * two_sig_x_sq) + np.sin(2*theta) / (2 * two_sig_y_sq)
+    c = sint_sq / two_sig_x_sq + cost_sq / two_sig_y_sq
+    g = offset + amplitude*np.exp( - (a*dx**2 + 2*b*dx*dy + c*dy**2))
+    return g.T.ravel()
+
+"""A simplified 2D gaussian function, used for fitting point sources"""
+def twoD_Gaussian_simple(xy, amplitude, xo, yo, sigma, offset):    
+    x, y = xy
+    dx, dy = x - xo, y - yo
+    a = 1 / (2 * sigma**2)
+    g = offset + amplitude*np.exp( - a*(dx**2 + dy**2))
+    return g.T.ravel()
+
+"""Function to automatically fit a gaussian, and return the best fit"""
+def fit_gauss(array, simple=False, mf=1000, debug=False):
+    xy = np.where(np.isfinite(array) & (array != 0))
+    
+    x = np.linspace(0, array.shape[0] - 1, array.shape[0])
+    y = np.linspace(0, array.shape[1] - 1, array.shape[1])
+    x, y = np.meshgrid(x, y)
+    
+    mx, mn = np.nanmax(array),  np.nanmin(array)
+    diff = mx - mn 
+    half_x, half_y = array.shape[0]/2, array.shape[1]/2
+    
+    if debug or simple:
+        # amplitude, x, y, sigma, z_offset
+        init = (mx, half_x, half_y, 1, mn)
+        bounds = [[mn, 0, 0, 0, -np.inf], [mx, array.shape[0], array.shape[1], np.inf, mx]]
+        popt, pcov = curve_fit(twoD_Gaussian_simple, xy, array[xy], p0=init, bounds=bounds, maxfev=mf)
+        
+        if debug: return twoD_Gaussian_simple((x, y), *popt).reshape((array.shape[0], array.shape[1])), popt, pcov
+        else:     return twoD_Gaussian_simple((x, y), *popt).reshape((array.shape[0], array.shape[1]))
+        
+    else:
+        # amplitude, x, y, sigma_x, sigma_y, theta, z_offset
+        init = (diff, half_x, half_y, 1, 1, 0, 0)
+        bounds = [[0, half_x-10, half_y-10, 0, 0, 0, -np.inf], [2 * diff, half_x+10, half_y+10, np.inf, np.inf, 2*np.pi, mx]]
+        popt, _ = curve_fit(twoD_Gaussian, xy, array[xy], p0=init, bounds=bounds, maxfev=mf)
+        return twoD_Gaussian((x, y), *popt).reshape((array.shape[0], array.shape[1]))
+    
+"""Volume under a 2D gaussian """
+def gaussian_volume(A, sx, sy=None):
+    if sy==None: return A * sx**2 * 2 * np.pi
+    else:        return A * sx * sy * 2 * np.pi
