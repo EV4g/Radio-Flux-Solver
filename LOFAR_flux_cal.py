2b9f10ef4a05be14a01306277c028a2810599b4f
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-02 11:40:41 +0100

    Multithreading update

--- LOFAR_flux_cal.py
+++ LOFAR_flux_cal.py
@@ -9,8 +9,10 @@ import glob
 import os
 from astropy.nddata.utils import Cutout2D
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 
-from functions import spectral_index, get_pixscale, generate_new_wcs, fit_gauss, gaussian_volume
+from functions import get_pixscale, generate_new_wcs, fit_gauss, gaussian_volume
 
 def line(x, a, b): 
     return a * x + b
@@ -18,6 +20,9 @@ def line(x, a, b):
 def log_linspace(mn, mx, n):
     return 10**np.linspace(np.log10(mn), np.log10(mx), n)
     
+def _worker(args):
+    ra, dec = args
+    return get_flux_fixed(ra, dec)
 
 racs_files = np.sort(glob.glob(os.getcwd()+"/data/racs/*.fits"))
 lofar_files = glob.glob(os.getcwd()+"/data/lofar_images/*.fits")
@@ -39,68 +44,53 @@ wcs_L = WCS(lhdul[0].header).celestial
 # get calagogue ra, dec
 cat_ra, cat_dec = cat.data['RA'], cat.data['DEC']
 
-# arrays to fill with values
-lofar_flux       = np.zeros_like(cat_ra)
-racs_flux        = np.zeros_like(cat_ra)
-peak_separation  = np.zeros_like(cat_ra)
-sigma_pcov_lofar = np.zeros_like(cat_ra)
-sigma_pcov_racs  = np.zeros_like(cat_ra)
-local_snr        = np.zeros_like(cat_ra)
-local_snr_fit    = np.zeros_like(cat_ra)
-
-# cutout width x height, and pixel and beam scaling
 w, h = 1.5 * u.arcmin, 1.5 * u.arcmin
-pix_per_deg, nx, ny = get_pixscale(wcs_R, wcs_L, w, h) # pix_per_deg is degrees
-beam_area_lofar_px = (np.pi / (4 * np.log(2))) * (lhdul[0].header['BMAJ'] / pix_per_deg.value) * (lhdul[0].header['BMIN'] / pix_per_deg.value)
-beam_area_racs_px  = (np.pi / (4 * np.log(2))) * (hdul[0].header['BMAJ'] / pix_per_deg.value) * (hdul[0].header['BMIN'] / pix_per_deg.value)
 
-for i, (ra, dec) in tqdm(enumerate(zip(cat_ra, cat_dec)), total=len(cat_ra)):    
+def get_flux(w, h, data1, data2, header1, header2, ra, dec):
+    wcs1 = WCS(header1).celestial
+    wcs2 = WCS(header2).celestial
+    pix_per_deg, nx, ny = get_pixscale(wcs1, wcs2, w, h)
+
+    beam_area_1 = (np.pi / (4*np.log(2))) * (header1['BMAJ'] / pix_per_deg.value) * (header1['BMIN'] / pix_per_deg.value)
+    beam_area_2 = (np.pi / (4*np.log(2))) * (header2['BMAJ'] / pix_per_deg.value) * (header2['BMIN'] / pix_per_deg.value)
+
     pos = SkyCoord(ra*u.deg, dec*u.deg, frame="icrs")
 
-    # cutout each image around (ra,dec) in its native grid (fast)
-    r_co = Cutout2D(racs_data,  position=pos, size=(h, w), wcs=wcs_R, mode="partial", fill_value=np.nan)
-    l_co = Cutout2D(lofar_data, position=pos, size=(h, w), wcs=wcs_L, mode="partial", fill_value=np.nan)
-    
-    # build common output WCS centered on (ra,dec) with your chosen nx,ny,pix_per_deg
-    wcs_out = generate_new_wcs(pos, nx, ny, pix_per_deg)
-    
-    # reproject both cutouts onto the same grid
-    racs_cut, racs_fp = reproject_interp((r_co.data, r_co.wcs), wcs_out, shape_out=(nx, ny))
-    lofar_cut, lofar_fp = reproject_interp((l_co.data, l_co.wcs), wcs_out, shape_out=(nx, ny))
-    
-    # gaussian point fit popt=[amplitude, x, y, sigma, z_offset]
-    lofar_fit, lofar_popt, lofar_pcov = fit_gauss(lofar_cut, simple=True, debug=True)
-    racs_fit,  racs_popt,  racs_pcov  = fit_gauss(racs_cut, simple=True, debug=True)
-    
-    local_snr[i]     = np.nanmax(lofar_cut) / np.nanstd(lofar_cut) # S/N of actual data
-    local_snr_fit[i] = np.nanmax(lofar_fit) / np.nanstd(lofar_cut) # S/N of fitting gaussian relative to data noise
-    
-    sigma_pcov_lofar[i] = np.sqrt(lofar_pcov[3,3])
-    sigma_pcov_racs[i]  = np.sqrt(racs_pcov[3,3])
-    
-    dist = np.sqrt((lofar_popt[1] - racs_popt[1])**2 + (lofar_popt[2] - racs_popt[2])**2)
-    peak_separation[i] = dist
-    
-    lofar_flux[i] = gaussian_volume(lofar_popt[0], lofar_popt[3]) / beam_area_lofar_px
-    racs_flux[i]  = gaussian_volume(racs_popt[0], racs_popt[3]) / beam_area_racs_px
-    
-    # debug plot
-    # is_valid = (lofar_flux[i] > 1e-3) & (racs_flux[i] > 1e-3) & np.isfinite(lofar_flux[i]) & np.isfinite(racs_flux[i]) & (dist < 2) & (sigma_pcov_lofar[i] < 0.3) & (sigma_pcov_racs[i] < 0.3)
-    # if is_valid:
-    #     fig, ax = plt.subplots(2, 2, figsize=(8, 8))
-    #     ax[0, 0].imshow(racs_cut, origin="lower", vmin=-np.nanstd(racs_cut), vmax=5*np.nanstd(racs_cut)); ax[0, 0].set_title("RACS")
-    #     ax[1, 0].imshow(lofar_cut, origin="lower", vmin=-np.nanstd(lofar_cut), vmax=5*np.nanstd(lofar_cut)); ax[1, 0].set_title("LOFAR")
-    #     ax[0, 1].imshow(racs_fit, origin="lower", vmin=-np.nanstd(racs_cut), vmax=5*np.nanstd(racs_cut))
-    #     ax[1, 1].imshow(lofar_fit, origin="lower", vmin=-np.nanstd(lofar_cut), vmax=5*np.nanstd(lofar_cut))
-    #     plt.suptitle(f"Source {i} is Valid: {is_valid}")
-    #     plt.show()
+    cutout1 = Cutout2D(data1, position=pos, size=(h, w), wcs=wcs1, mode="partial", fill_value=np.nan)
+    cutout2 = Cutout2D(data2, position=pos, size=(h, w), wcs=wcs2, mode="partial", fill_value=np.nan)
 
+    wcs_out = generate_new_wcs(pos, nx, ny, pix_per_deg)
+
+    reproj1, _ = reproject_interp((cutout1.data, cutout1.wcs), wcs_out, shape_out=(nx, ny))
+    reproj2, _ = reproject_interp((cutout2.data, cutout2.wcs), wcs_out, shape_out=(nx, ny))
+
+    fit1, popt1, pcov1 = fit_gauss(reproj1, simple=True, debug=True)
+    fit2, popt2, pcov2 = fit_gauss(reproj2, simple=True, debug=True)
+
+    local_snr     = np.nanmax(reproj1) / np.nanstd(reproj1)
+    local_snr_fit = np.nanmax(reproj2) / np.nanstd(reproj2)
+
+    sigma_pcov1 = np.sqrt(pcov1[3, 3])
+    sigma_pcov2 = np.sqrt(pcov2[3, 3])
+
+    dist = np.sqrt((popt1[1] - popt2[1])**2 + (popt1[2] - popt2[2])**2)
+
+    flux1 = gaussian_volume(popt1[0], popt1[3]) / beam_area_1  # ← fixed: was popt2[0]
+    flux2 = gaussian_volume(popt2[0], popt2[3]) / beam_area_2
+
+    return flux1, flux2, dist, sigma_pcov1, sigma_pcov2, local_snr, local_snr_fit
+
+get_flux_fixed = partial(get_flux, w, h, lofar_data, racs_data, lhdul[0].header, hdul[0].header)
+
+with ProcessPoolExecutor(max_workers=24) as executor:
+    results = list(tqdm(executor.map(_worker, zip(cat_ra, cat_dec), chunksize=8), total=len(cat_ra)))
+
+lofar_flux, racs_flux, peak_separation, sigma_pcov_lofar, sigma_pcov_racs, local_snr, local_snr_fit = map(np.array, zip(*results))
 
 # quality filtering
 valid = (lofar_flux > 1e-3) & (racs_flux > 1e-3) & np.isfinite(lofar_flux) & np.isfinite(racs_flux) \
         & (peak_separation < 2) & (sigma_pcov_lofar < 0.3) & (sigma_pcov_racs < 0.3) \
         & (local_snr > 5) & (local_snr_fit > 4)
-        
 
 #### analysis
 spectral_index_alpha = -0.7
@@ -128,7 +118,7 @@ print(f"Scatter (1σ)         : {scatter:.4f} dex")
 print(f"Uncertainty on median: ±{stderr:.4f} dex")
 
 fig, ax = plt.subplots(1, 2, figsize=(11, 4))
-ax[0].scatter(racs_flux[valid], lofar_flux[valid], s=10, alpha=0.7)
+ax[0].scatter(racs_flux[valid], lofar_flux[valid], s=10, alpha=0.7, c=local_snr[valid])
 ax[0].plot(x, x * index, color='purple', ls='--', label="Data fit")
 ax[0].plot(x, x, color='black', ls='--', label="x = y")
 ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_alpha})')
