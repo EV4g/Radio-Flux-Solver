3b2f61b72320bde6d036cbd35ce4d39319236aa0
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-04 14:39:38 +0100

    Update fits_overlap_grabber.py

--- fits_overlap_grabber.py
+++ fits_overlap_grabber.py
@@ -1,17 +1,7 @@
-import warnings
-from astropy.wcs import FITSFixedWarning
-from astropy.io import fits
-from astropy.wcs import WCS
-from astropy.coordinates import SkyCoord
-import astropy.units as u
 import numpy as np
-from reproject import reproject_interp
-import matplotlib.pyplot as plt
 import glob
+from functions import get_overlapping_files
 
-from functions import get_survey_file
-
-warnings.filterwarnings("ignore", category=FITSFixedWarning)
 basedir = "/home/floris/Documents/PhD/Galactic plane/LOFAR_and_MeerKAT/data/"
 
 meerkat_files = np.sort(glob.glob(basedir+"meerkat/*.fits"))
@@ -19,10 +9,6 @@ racs_files = np.sort(glob.glob(basedir+"racs/*.fits"))
 vlssr_files = np.sort(glob.glob(basedir+"vlssr/*.fits"))
 lofar_files = np.sort(glob.glob(basedir+"lofar/*.fits"))
 
-
-pos = SkyCoord(282.00*u.deg, 0.00*u.deg, frame="icrs")
-
-
-print(get_survey_file(meerkat_files, pos))
-print(get_survey_file(racs_files, pos))
-print(get_survey_file(vlssr_files, pos))
+print(get_overlapping_files(lofar_files[0], meerkat_files), "\n")
+print(get_overlapping_files(lofar_files[0], racs_files), "\n")
+print(get_overlapping_files(lofar_files[0], vlssr_files), "\n")
