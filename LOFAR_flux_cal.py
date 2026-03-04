b255682dbdbf9e0fef6306d5f1ca0cc35093fee3
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-04 13:44:58 +0100

    Update LOFAR_flux_cal.py

--- LOFAR_flux_cal.py
+++ LOFAR_flux_cal.py
@@ -7,8 +7,12 @@ import os
 #import multiprocessing; multiprocessing.set_start_method('fork') #for windows/mac
 from functions import get_flux_batch, prep_file, compute_fluxcal_statistics
 
-def line(x, a, b): 
-    return a * x + b
+"""Plot (log)ratio as function of position in field."""
+def plot_location_dependant_index(ra, dec, ratio):
+    plt.scatter(ra, dec, c=ratio)
+    plt.colorbar()
+    plt.gca().set_box_aspect(1)
+    plt.show()
 
 spectral_index_theory = -0.7
 lofar_freq = 144.6e6 #Hz
@@ -63,36 +67,36 @@ plt.show()
 
 
 
-######## LOFAR v MEERKAT ########
-#### calculate fluxes for all valid catalogue sources
-lofar_flux, meerkat_flux, peak_separation, sigma_pcov_lofar, sigma_pcov_meerkat, local_snr, local_snr_fit = get_flux_batch(w, h, lofar_data, meerkat_data, lofar_header, meerkat_header, cat_ra, cat_dec)
+# ######## LOFAR v MEERKAT ########
+# #### calculate fluxes for all valid catalogue sources
+# lofar_flux, meerkat_flux, peak_separation, sigma_pcov_lofar, sigma_pcov_meerkat, local_snr, local_snr_fit = get_flux_batch(w, h, lofar_data, meerkat_data, lofar_header, meerkat_header, cat_ra, cat_dec)
 
-#### quality filtering
-valid = (lofar_flux > 1e-3) & (meerkat_flux > 5e-3) & np.isfinite(lofar_flux) & np.isfinite(meerkat_flux) \
-        & (peak_separation < 2) & (sigma_pcov_lofar < 0.3) & (sigma_pcov_meerkat < 0.3) \
-        & (local_snr > 5) & (local_snr_fit > 4)
+# #### quality filtering
+# valid = (lofar_flux > 1e-3) & (meerkat_flux > 5e-3) & np.isfinite(lofar_flux) & np.isfinite(meerkat_flux) \
+#         & (peak_separation < 2) & (sigma_pcov_lofar < 0.3) & (sigma_pcov_meerkat < 0.3) \
+#         & (local_snr > 5) & (local_snr_fit > 4)
 
-#### analysis
-spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(lofar_freq, meerkat_freq, lofar_flux, meerkat_flux, valid=valid)
+# #### analysis
+# spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(lofar_freq, meerkat_freq, lofar_flux, meerkat_flux, valid=valid)
 
-fig, ax = plt.subplots(1, 2, figsize=(11, 4))
-ax[0].scatter(meerkat_flux[valid], lofar_flux[valid], s=10, alpha=0.7, c=local_snr[valid])
-ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
-ax[0].plot(x, x, color='black', ls='--', label="x = y")
-ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
-ax[0].set_xscale('log'); ax[0].set_yscale('log')
-ax[0].set_xlabel("MeerKat flux [Jy]"); ax[0].set_ylabel("LOFAR flux [Jy]")
-ax[0].legend()
+# fig, ax = plt.subplots(1, 2, figsize=(11, 4))
+# ax[0].scatter(meerkat_flux[valid], lofar_flux[valid], s=10, alpha=0.7, c=local_snr[valid])
+# ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
+# ax[0].plot(x, x, color='black', ls='--', label="x = y")
+# ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
+# ax[0].set_xscale('log'); ax[0].set_yscale('log')
+# ax[0].set_xlabel("MeerKat flux [Jy]"); ax[0].set_ylabel("LOFAR flux [Jy]")
+# ax[0].legend()
 
-# Log-ratio histogram
-ax[1].hist(log_ratio, bins=15, edgecolor='k')
-ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
-ax[1].set_xlabel(r"log$_{10}$(S_LOFAR / S_MeerKat_corrected)")
-ax[1].set_ylabel("N")
-ax[1].legend()
-plt.tight_layout()
-#plt.savefig("flux_scale_comparison.png", dpi=150)
-plt.show()
+# # Log-ratio histogram
+# ax[1].hist(log_ratio, bins=15, edgecolor='k')
+# ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
+# ax[1].set_xlabel(r"log$_{10}$(S_LOFAR / S_MeerKat_corrected)")
+# ax[1].set_ylabel("N")
+# ax[1].legend()
+# plt.tight_layout()
+# #plt.savefig("flux_scale_comparison.png", dpi=150)
+# plt.show()
 
 
 
