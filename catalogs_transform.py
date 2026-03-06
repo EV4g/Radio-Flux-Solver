0d65fb5d4d852fb2ba11ffe5979f404cfaec01da
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-06 16:02:43 +0100

    Update catalogs_transform.py

--- catalogs_transform.py
+++ catalogs_transform.py
@@ -11,6 +11,7 @@ from astropy.table import Table
 racs    = Table.read(os.getcwd()+"/catalogs/racs/RACS_DR1_Sources_GalacticRegion_v2021_08.xml")
 meerkat = Table.read(os.getcwd()+"/catalogs/meerkat/smgps_moment0_5beam_5sigma_510599row_compact_source_catalogue.csv")
 vlssr   = Table.read(os.getcwd()+"/catalogs/vlssr/vlssr_full.csv")
+tgss    = Table.read(os.getcwd()+"/catalogs/tgss/TGSSADR1_7sigma_catalog.fits")
 
 # from astroquery.vizier import Vizier
 # v = Vizier(row_limit=-1)
@@ -20,31 +21,33 @@ vlssr   = Table.read(os.getcwd()+"/catalogs/vlssr/vlssr_full.csv")
 
 
 #### racs ####
-racs['flux_jy'] = racs['Total_flux_Source'] * 1e-3
-racs['e_flux_jy'] = racs['E_Total_flux_Source'] * 1e-3
-racs.rename_column("RA", "ra")
-racs.rename_column("Dec", "dec")
-racs_out = racs["Source_Name", "ra", "dec", "flux_jy", "e_flux_jy"]
-racs_out.write("racs_clean.csv", format="ascii.csv", overwrite=True)
+# racs['flux_jy'] = racs['Total_flux_Source'] * 1e-3
+# racs['e_flux_jy'] = racs['E_Total_flux_Source'] * 1e-3
+# racs.rename_column("RA", "ra")
+# racs.rename_column("Dec", "dec")
+# racs_out = racs["Source_Name", "ra", "dec", "flux_jy", "e_flux_jy"]
+# racs_out.write("racs_clean.csv", format="ascii.csv", overwrite=True)
 
 #### meerkat ####
-meerkat['flux_jy'] = meerkat['int_flux'] * 1e-3
-meerkat['e_flux_jy'] = meerkat['err_int_flux'] * 1e-3
-meerkat_out = meerkat["csc_id", "ra", "dec", "snr", "flux_jy", "e_flux_jy"]
-meerkat_out.write("meerkat_clean.csv", format="ascii.csv", overwrite=True)
+# meerkat['flux_jy'] = meerkat['int_flux'] * 1e-3
+# meerkat['e_flux_jy'] = meerkat['err_int_flux'] * 1e-3
+# meerkat_out = meerkat["csc_id", "ra", "dec", "snr", "flux_jy", "e_flux_jy"]
+# meerkat_out.write("meerkat_clean.csv", format="ascii.csv", overwrite=True)
 
 #### vlssr ####
-DEG_TO_ARCSEC = 3600.0
-VLSSR_BEAM_ARCSEC = 80.0  # circular restoring beam
+# DEG_TO_ARCSEC = 3600.0
+# VLSSR_BEAM_ARCSEC = 80.0  # circular restoring beam
 
-src_maj = vlssr["MajAx"] * DEG_TO_ARCSEC
-src_min = vlssr["MinAx"] * DEG_TO_ARCSEC
+# src_maj = vlssr["MajAx"] * DEG_TO_ARCSEC
+# src_min = vlssr["MinAx"] * DEG_TO_ARCSEC
 
-vlssr['flux_jy'] = vlssr["Sp"] * (src_maj * src_min) / (VLSSR_BEAM_ARCSEC ** 2)
-vlssr['e_flux_jy'] = vlssr["e_Sp"] * (src_maj * src_min) / (VLSSR_BEAM_ARCSEC ** 2)
+# vlssr['flux_jy'] = vlssr["Sp"] * (src_maj * src_min) / (VLSSR_BEAM_ARCSEC ** 2)
+# vlssr['e_flux_jy'] = vlssr["e_Sp"] * (src_maj * src_min) / (VLSSR_BEAM_ARCSEC ** 2)
 
-vlssr.rename_column("ra_deg", "ra")
-vlssr.rename_column("dec_deg", "dec")
+# vlssr.rename_column("ra_deg", "ra")
+# vlssr.rename_column("dec_deg", "dec")
 
-vlssr_out = vlssr["ra", "dec", "flux_jy", 'e_flux_jy']
-vlssr_out.write("vlssr_clean.csv", format="ascii.csv", overwrite=True)
+# vlssr_out = vlssr["ra", "dec", "flux_jy", 'e_flux_jy']
+# vlssr_out.write("vlssr_clean.csv", format="ascii.csv", overwrite=True)
+
+#### tgss ####
