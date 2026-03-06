da0d65edf53d16e1a66f0bd34b2bae2d26280d3d
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-06 16:32:57 +0100

    EVERYTHING into a function

--- LOFAR_flux_cal_catalog.py
+++ LOFAR_flux_cal_catalog.py
@@ -45,11 +45,38 @@ def get_catalog_matched_flux(cat1, cat2):
     
     return flux1, flux2, snr1, snr2
 
+"""Load two catalogs and plot their relative fluxes, according to a powerlaw"""
+def quick_compare_catalog(cat1, cat2, freq1, freq2, name1, name2):
+    flux1, flux2, _, _ = get_catalog_matched_flux(cat1, cat2)
+    spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(freq1, freq2, flux1, flux2)
+
+    fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+    ax[0].scatter(flux2, flux1, s=10, alpha=0.7)
+    ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
+    ax[0].plot(x, x, color='black', ls='--', label="x = y")
+    ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
+    ax[0].set_xscale('log'); ax[0].set_yscale('log')
+    ax[0].set_xlabel(f"{name1} flux [Jy]"); ax[0].set_ylabel(f"{name2} flux [Jy]")
+    ax[0].legend()
+
+    # Log-ratio histogram
+    ax[1].hist(log_ratio, bins=30, edgecolor='k')
+    ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
+    ax[1].set_xlabel(f"log10(S_{name1} / S_{name2})")
+    ax[1].set_ylabel("N")
+    ax[1].legend()
+    plt.tight_layout()
+    #plt.savefig("flux_scale_comparison.png", dpi=150)
+    plt.show()
+    
+    print(f"Compared {name1} to {name2}")
+    
 spectral_index_theory = -0.7
 lofar_freq   = 144.6e6 #Hz
 racs_freq    = 887.5e6 #Hz
 meerkat_freq = 1367e6  #Hz
 vlssr_freq   = 73.8e6  #Hz
+tgss_freq    = 150e6   #Hz
 
 lofar_files = np.sort(glob.glob(os.getcwd()+"/data/lofar/*.fits"))[0]
 
@@ -65,6 +92,7 @@ lofar_files = np.sort(glob.glob(os.getcwd()+"/data/lofar/*.fits"))[0]
 racs_full    = Table.read(os.getcwd()+"/catalogs/racs/racs_clean.csv")
 meerkat_full = Table.read(os.getcwd()+"/catalogs/meerkat/meerkat_clean.csv")
 vlssr_full   = Table.read(os.getcwd()+"/catalogs/vlssr/vlssr_clean.csv")
+tgss_full    = Table.read(os.getcwd()+"/catalogs/tgss/tgss_clean.fits")
 lofar        = Table.read(os.getcwd()+"/catalogs/lofar/lofar_sources.fits")
 
 # fix lofar
@@ -77,22 +105,31 @@ lofar.rename_column('E_Total_flux', 'e_flux_jy')
 racs_valid      = sources_in_fits(racs_full['ra'],    racs_full['dec'],    lofar_files)
 meerkat_valid   = sources_in_fits(meerkat_full['ra'], meerkat_full['dec'], lofar_files)
 vlssr_valid     = sources_in_fits(vlssr_full['ra'],   vlssr_full['dec'],   lofar_files)
+tgss_valid      = sources_in_fits(tgss_full['ra'],    tgss_full['dec'],    lofar_files)
+
+# remove all non-valid points to reduce syntax clutter
+racs    = racs_full[racs_valid]
+meerkat = meerkat_full[meerkat_valid]
+vlssr   = vlssr_full[vlssr_valid]
+tgss    = tgss_full[tgss_valid]
 
 # catalog plot
-plt.hist(np.log10(racs_full['flux_jy']), alpha=0.6, bins=100, label='racs')
+plt.hist(np.log10(racs_full['flux_jy']),    alpha=0.6, bins=100, label='racs')
 plt.hist(np.log10(meerkat_full['flux_jy']), alpha=0.6, bins=100, label='meerkat')
-plt.hist(np.log10(vlssr_full['flux_jy']), alpha=0.6, bins=100, label='vlssr')
-plt.hist(np.log10(lofar['Total_flux']), alpha=0.6, bins=50, label='lofar')
+plt.hist(np.log10(vlssr_full['flux_jy']),   alpha=0.6, bins=100, label='vlssr')
+plt.hist(np.log10(tgss_full['flux_jy']),    alpha=0.6, bins=100, label='tgss')
+plt.hist(np.log10(lofar['flux_jy']),        alpha=0.6, bins=50,  label='lofar')
 plt.xlabel("log10(flux/Jy)")
 plt.ylabel("count")
 plt.yscale('log')
 plt.legend()
 plt.show()
 
-plt.hist(np.log10(racs_full['flux_jy'][racs_valid]), alpha=0.6, bins=20, label='racs_v')
-plt.hist(np.log10(meerkat_full['flux_jy'][meerkat_valid]), alpha=0.6, bins=20, label='meerkat_v')
-plt.hist(np.log10(vlssr_full['flux_jy'][vlssr_valid]), alpha=0.6, bins=20, label='vlssr_v')
-plt.hist(np.log10(lofar['Total_flux']), alpha=0.6, bins=50, label='lofar')
+plt.hist(np.log10(racs['flux_jy']),    alpha=0.6, bins=20, label='racs_v')
+plt.hist(np.log10(meerkat['flux_jy']), alpha=0.6, bins=20, label='meerkat_v')
+plt.hist(np.log10(vlssr['flux_jy']),   alpha=0.6, bins=20, label='vlssr_v')
+plt.hist(np.log10(tgss['flux_jy']),    alpha=0.6, bins=100, label='tgss_v')
+plt.hist(np.log10(lofar['flux_jy']),   alpha=0.6, bins=50, label='lofar')
 plt.legend()
 plt.xlabel("log10(flux/Jy)")
 plt.ylabel("count")
@@ -100,90 +137,25 @@ plt.yscale('log')
 plt.show()
 
 # plot catalog as function of position
-plt.scatter(racs_full['ra'][racs_valid], racs_full['dec'][racs_valid], s=2)
-plt.scatter(meerkat_full['ra'][meerkat_valid], meerkat_full['dec'][meerkat_valid], s=2)
-plt.scatter(vlssr_full['ra'][vlssr_valid], vlssr_full['dec'][vlssr_valid], s=2)
-plt.scatter(lofar['RA'], lofar['DEC'], s=1)
+plt.scatter(racs['ra'],    racs['dec'],    s=2, label='racs')
+plt.scatter(meerkat['ra'], meerkat['dec'], s=2, label='meerkat')
+plt.scatter(vlssr['ra'],   vlssr['dec'],   s=2, label='vlssr')
+plt.scatter(tgss['ra'],    tgss['dec'],    s=2, label='tgss')
+plt.scatter(lofar['ra'],   lofar['dec'],   s=1, label='lofar')
 plt.gca().set_box_aspect(1)
 plt.xlabel("RA")
 plt.ylabel("Dec")
+plt.legend()
 plt.show()
 
-# remove all non-valid points to reduce syntax clutter
-racs    = racs_full[racs_valid]
-meerkat = meerkat_full[meerkat_valid]
-vlssr   = vlssr_full[vlssr_valid]
 
 
 #### analysis
 # meerkat v lofar
-lofar_flux, meerkat_flux, lofar_snr, meerkat_snr = get_catalog_matched_flux(lofar, meerkat)
-spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(lofar_freq, meerkat_freq, lofar_flux, meerkat_flux)
-
-fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-ax[0].scatter(meerkat_flux, lofar_flux, s=10, alpha=0.7)
-ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
-ax[0].plot(x, x, color='black', ls='--', label="x = y")
-ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
-ax[0].set_xscale('log'); ax[0].set_yscale('log')
-ax[0].set_xlabel("Meerkat flux [Jy]"); ax[0].set_ylabel("LOFAR flux [Jy]")
-ax[0].legend()
-
-# Log-ratio histogram
-ax[1].hist(log_ratio, bins=30, edgecolor='k')
-ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
-ax[1].set_xlabel(r"log$_{10}$(S_LOFAR / S_Meerkat_corrected)")
-ax[1].set_ylabel("N")
-ax[1].legend()
-plt.tight_layout()
-#plt.savefig("flux_scale_comparison.png", dpi=150)
-plt.show()
-
+quick_compare_catalog(lofar, meerkat, lofar_freq, meerkat_freq, "lofar", "meerkat")
 
 # racs v lofar
-lofar_flux, racs_flux, lofar_snr, racs_snr = get_catalog_matched_flux(lofar, racs)
-spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(lofar_freq, racs_freq, lofar_flux, racs_flux)
-
-fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-ax[0].scatter(racs_flux, lofar_flux, s=10, alpha=0.7)
-ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
-ax[0].plot(x, x, color='black', ls='--', label="x = y")
-ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
-ax[0].set_xscale('log'); ax[0].set_yscale('log')
-ax[0].set_xlabel("RACS flux [Jy]"); ax[0].set_ylabel("LOFAR flux [Jy]")
-ax[0].legend()
-
-# Log-ratio histogram
-ax[1].hist(log_ratio, bins=30, edgecolor='k')
-ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
-ax[1].set_xlabel(r"log$_{10}$(S_LOFAR / S_RACS_corrected)")
-ax[1].set_ylabel("N")
-ax[1].legend()
-plt.tight_layout()
-#plt.savefig("flux_scale_comparison.png", dpi=150)
-plt.show()
-
-
+quick_compare_catalog(lofar, racs, lofar_freq, racs_freq, "lofar", "racs")
 
 # racs v meerkat
-racs_flux, meerkat_flux, racs_snr, meerkat_snr = get_catalog_matched_flux(racs, meerkat)
-spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(meerkat_freq, racs_freq, meerkat_flux, racs_flux)
-
-fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-ax[0].scatter(racs_flux, meerkat_flux, s=10, alpha=0.7)
-ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
-ax[0].plot(x, x, color='black', ls='--', label="x = y")
-ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={spectral_index_theory})')
-ax[0].set_xscale('log'); ax[0].set_yscale('log')
-ax[0].set_xlabel("RACS flux [Jy]"); ax[0].set_ylabel("meerkat flux [Jy]")
-ax[0].legend()
-
-# Log-ratio histogram
-ax[1].hist(log_ratio, bins=30, edgecolor='k')
-ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
-ax[1].set_xlabel(r"log$_{10}$(S_meerkat / S_RACS_corrected)")
-ax[1].set_ylabel("N")
-ax[1].legend()
-plt.tight_layout()
-#plt.savefig("flux_scale_comparison.png", dpi=150)
-plt.show()
+quick_compare_catalog(racs, meerkat, racs_freq, meerkat_freq, "racs", "meerkat")
