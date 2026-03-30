3eeafb701bf9bdffdf9755de51b9121a86eb1fc3
Floris <49408883+EV4g@users.noreply.github.com>
2026-03-30 16:48:00 +0200

    moving all functions to functions.py

--- LOFAR_flux_cal_catalog.py
+++ LOFAR_flux_cal_catalog.py
@@ -1,206 +1,11 @@
-from astropy.io import fits
-from astropy.wcs import WCS
-import astropy.units as u
-from astropy.coordinates import SkyCoord
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
-from functions import match_catalogs_2D, compute_fluxcal_statistics, get_spectral_index, calculate_contour_statistics, get_combinations, get_pos_err_deg, weighted_bin_stats, weighted_bin_stats_2d, sources_in_fits
-from astropy.table import Table
-from scipy.stats import chi2
-from itertools import combinations
+from functions import match_catalogs_2D, compute_fluxcal_statistics, get_spectral_index, calculate_contour_statistics, get_combinations, weighted_bin_stats, weighted_bin_stats_2d
+from functions import compute_flux_correction_factor, calculate_correction_factor_weight, calculate_1d_peak
 from time import perf_counter
-
 from catalog_manager import catalog, config, catalog_set
-
-"""Get two catalogs and return flux and snr (flux / e_flux)"""
-def get_catalog_matched_flux(cat1, cat2, thres_arc=2):
-    idx_cat1, idx_cat2 = match_catalogs_2D([cat1, cat2], thres_arc=thres_arc)
-
-    flux1 = cat1.flux[idx_cat1]
-    snr1 = flux1 / cat1.e_flux[idx_cat1]
-
-    flux2 = cat2.flux[idx_cat2]
-    snr2 = flux2 / cat2.e_flux[idx_cat2]
-
-    return flux1, flux2, snr1, snr2
-
-"""Load two catalogs and plot their relative fluxes, according to a powerlaw"""
-def quick_compare_catalog(cat1, cat2, config):
-    flux1, flux2, _, _ = get_catalog_matched_flux(cat1, cat2, thres_arc=config.thres_arc)
-    spectral_flux_ratio, spectral_index_actual, x, log_ratio, scale_factor = compute_fluxcal_statistics(cat1.freq, cat2.freq, flux1, flux2, config.spectral_index_theory)
-
-    fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-    ax[0].scatter(flux2, flux1, s=10, alpha=0.7)
-    ax[0].plot(x, x * spectral_index_actual, color='purple', ls='--', label="Data fit")
-    ax[0].plot(x, x, color='black', ls='--', label="x = y")
-    ax[0].plot(x, x * spectral_flux_ratio, 'r--', label=f'Expected (α={config.spectral_index_theory})')
-    ax[0].set_xscale('log'); ax[0].set_yscale('log')
-    ax[0].set_xlabel(f"{cat1.name} flux [Jy]"); ax[0].set_ylabel(f"{cat2.name} flux [Jy]")
-    ax[0].legend()
-    
-    # Log-ratio histogram
-    ax[1].hist(log_ratio, bins=30, edgecolor='k')
-    ax[1].axvline(scale_factor, color='red', ls='--', label=f'Median = {scale_factor:.3f}')
-    ax[1].set_xlabel(f"log10(S_{cat1.name} / S_{cat2.name})")
-    ax[1].set_ylabel("N")
-    ax[1].legend()
-    plt.tight_layout()
-    #plt.savefig("flux_scale_comparison.png", dpi=150)
-    plt.show()
-
-    print(f"Compared {cat1.name} to {cat2.name} \n")
-
-"""compute the flux correction factor based on three given catalogs. Catalogs are matches, and the last two are used to calculate the spectral index
-which is used to extrapolate what the first cat -should- be. The different between -should- and -is-, is the correction factor."""
-def compute_flux_correction_factor(cats, config, debug=False, internal_output=False, anchor_override=None):
-    indices, quality = match_catalogs_2D(cats, thres_arc=config.thres_arc, return_quality=True, nsigma=config.nsigma, thres_arc_override=config.thres_arc_override, crowd_radius_arc=config.crowd_radius_arc)
-
-    # if there are too few sources, return None
-    if len(indices[0]) <= config.minimum_points:
-        if internal_output: print(f"Error: no source-matches found between [{', '.join(f'{cat.name}' for cat in cats)}]")
-        return None
-
-    # figure out for which catalog the correction factor should be calculated
-    # if cat is given, match that, otherwise assume the first index
-    other_index = [i for i in range(len(cats))]
-    if config.anchor_catalog is None:
-        anchor_index = 0
-    else:
-        if anchor_override is None:
-            try:
-                anchor_index = cats.index(config.anchor_catalog)
-            except:
-                print("Anchor catalog missing from inputs")
-                return None
-        else:
-            anchor_index = anchor_override
-    other_index.remove(anchor_index)
-
-    # if crowding parameter is available, store max neighbour (withing crowding_radius) count per source
-    n_crowd = np.zeros(len(indices[0]), dtype=int)
-    if quality['n_crowd']:
-        for cat_idx, match_idx in enumerate(indices):
-            nc = quality['n_crowd'].get(cat_idx)
-            if nc is not None and len(nc):
-                n_crowd = np.maximum(n_crowd, nc[match_idx])
-    
-    # re-check after crowding
-    if len(indices[0]) <= config.minimum_points:
-        if internal_output: print(f"Error: no source-matches found between [{', '.join(f'{cat.name}' for cat in cats)}] after crowding check")
-        return None
-
-    # create subsets of all catalogs, such that we can ignore (i0,i1,...) afterwards
-    for index, cat in enumerate(cats): cats[index] = cat.create_subset(indices[index])
-
-    # all pairwise separations, then take per-source maximum
-    coords = [SkyCoord(ra=cat.ra * u.deg, dec=cat.dec * u.deg) for cat in cats]
-    sig    = [np.rad2deg(cat.err_rad) * 3600 for cat in cats]   # arcsec per-source
-
-    pair_seps = {}
-    p_weight  = np.ones(len(cats[0].ra))
-    for a, b in combinations(range(len(cats)), 2):
-        sep = coords[a].separation(coords[b]).arcsec
-        pair_seps[(a, b)] = sep
-        p_weight *= 1.0 - chi2.cdf((sep / np.hypot(sig[a], sig[b])) ** 2, df=2)
-
-    max_sep = np.maximum.reduce(list(pair_seps.values()))
-
-    # flux and flux error
-    uncorrected_flux = cats[anchor_index].flux
-    uncorrected_flux_error = cats[anchor_index].e_flux
-
-    # calculate spectral indices based on available data
-    # w.i.p. for case n=4, where curvature can be estimated
-    match len(cats):
-        case 2:
-            spectral_indices = np.ones_like(cats[0].flux) * config.spectral_index_theory
-        case 3:
-            spectral_indices = get_spectral_index(cats[other_index[0]].flux, cats[other_index[1]].flux, cats[other_index[0]].freq, cats[other_index[1]].freq)
-        case _:
-            print("Not yet implemented")
-            return None
-
-    # fit is based on measuring between two points, linear is average between assuming theoretical extragalactic value for both
-    extrapolated_flux_fit = get_flux_from_index(spectral_indices, cats[other_index[0]].flux, cats[anchor_index].freq, cats[other_index[0]].freq)
-    
-    # calculate the linear theoretical flux at anchor_index based on all the other catalogs, and average the result
-    extrapolated_flux_linear = np.mean([get_flux_from_index(config.spectral_index_theory, cats[index].flux, cats[anchor_index].freq, cats[index].freq) for index in other_index], axis=0)
-
-    if debug:
-        for flux in np.array([cat.flux for cat in cats]).T:
-            plt.plot(np.array([cat.freq for cat in cats])*1e-6, flux)
-        
-        plt.yscale('log')
-        plt.ylabel(r"log$_{10}$(Jy)")
-        plt.xlabel("Frequency (MHz)")
-        plt.show()
-        
-    # correction factor is the factor to multiply the anchor_catalog flux by to get what it should be, based on the other catalogs
-    correction_factor = extrapolated_flux_fit / uncorrected_flux
-    snr = uncorrected_flux / uncorrected_flux_error
-    
-    # apply weighting factor per catalog, downweighting uncertain ones. To be depracated in favour
-    # of a proper flux extrapolator and proper catalog uncertainty values.
-    catalog_weight_factor = np.ones_like(snr)
-    for cat in cats:
-        if cat.name.lower() == "tgss": catalog_weight_factor *= 0.5
-        if cat.name.lower() == "gleam_300": catalog_weight_factor *= 0.7
-        if cat.name.lower() == "gleam_xgp": catalog_weight_factor *= 0.8
-    
-    # calculate average position over all catalogs instead of using the first catalog
-    ra = np.average([cat.ra for cat in cats], axis=0)
-    dec = np.average([cat.dec for cat in cats], axis=0)
-    
-    if debug:
-        # compare -0.7 assumption versus fitted spectral indices
-        mn, mx = min(np.min(extrapolated_flux_fit), np.min(extrapolated_flux_linear)), max(np.max(extrapolated_flux_fit), np.max(extrapolated_flux_linear))
-        plt.scatter(extrapolated_flux_linear, extrapolated_flux_fit, c=spectral_indices)
-        plt.yscale('log')
-        plt.xscale('log')
-        plt.xlim(mn, mx)
-        plt.ylim(mn, mx)
-        plt.gca().set_box_aspect(1)
-        plt.plot((mn, mx), (mn, mx), c='k', ls='--')
-        plt.colorbar(label = r"Spectral index $\alpha$")
-        plt.xlabel(f"{cats[anchor_index].name} linear flux (Jy)")
-        plt.ylabel(f"{cats[anchor_index].name} fitted flux (Jy)")
-        plt.title(f"{cats[anchor_index].name} "+r"flux, $\alpha$=-0.7 vs fitted")
-        plt.show()
-        
-        # compare fitted spectral index with correction factor
-        plt.scatter(spectral_indices, correction_factor, c=uncorrected_flux, norm='log')
-        plt.yscale('log')
-        plt.axvline(-0.7, ls='--', c='k')
-        plt.axhline(1, ls='--', c='k')
-        plt.colorbar(label='Flux (Jy)')
-        plt.ylabel("Flux correction factor")
-        plt.xlabel(r"Spectral index $\alpha$")
-        plt.title("Flux correction as function of spectral index")
-        plt.show()
-        
-    if internal_output: print(f"Completed set [{', '.join(f'{cat.name:9}' for cat in cats)}]", f"Matches: {len(indices[0])}")    
-    
-    return (spectral_indices, snr, correction_factor, extrapolated_flux_fit, catalog_weight_factor, max_sep, p_weight, n_crowd, ra, dec)
-
-"""Calculate weighted correction factor based on per-point spectral indices, signal-to-noise, and correction factor"""
-def calculate_correction_factor_weight(spx, snr, catw, max_sep, p_match, n_crowd, config):
-    # downweight sources with spectral indices far away from -0.7
-    spectral_difference_factor = np.exp(-config.spectral_damping_factor * (spx - config.spectral_index_theory)**2)
-    
-    # discard low snr sources
-    signal_to_noise_factor = snr.copy()
-    signal_to_noise_factor[signal_to_noise_factor < config.snr_lower_limit] = 0
-    
-    # weighting based on separation between points
-    separation_weight = np.exp(-(max_sep / config.thres_arc) ** 2)
-    
-    return spectral_difference_factor * signal_to_noise_factor * catw * separation_weight * p_match
-
-"""Extrapolate flux based on two frequencies, one flux, and a spectral index"""
-def get_flux_from_index(spectral_index, reference_flux, current_frequency, reference_frequency):
-    return reference_flux * (current_frequency / reference_frequency) ** spectral_index
 
 start = perf_counter()
 
@@ -257,21 +62,21 @@ cygnus_config = config(spectral_damping_factor = 5,
                        )
 
 small_config = config(spectral_damping_factor = 5,
-                       snr_lower_limit = 7,
+                       snr_lower_limit = 0,
                        nsigma = 3,
                        minimum_points = 3,
                        crowd_radius_arc = None,
                        minimum_frequency_spacing = None,
-                       catalogs = [lofar_dr3, nvss, racs],
+                       catalogs = [wenss, nvss, racs_gal, racs],
                        reference_file = None,
-                       anchor_catalog = racs,
+                       anchor_catalog = wenss,
                        )
 
 #### Parameters
 debug = False
-#config = lofar_dr3_config
+config = lofar_dr3_config
 #config = default_config
-config = cygnus_config
+#config = cygnus_config
 #config = small_config
 
 config.setup()
